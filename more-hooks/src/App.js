03f30319563801dd8963e5e42d3ea84ff6cb84e8
Wallmonger <nicolas.berrier360@gmail.com>
2023-10-09 18:23:14 +0200

    Memoïze values (memo)

--- more-hooks/src/App.js
+++ more-hooks/src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import Profile from './components/Profile';
 import axios from 'axios';
 
@@ -6,6 +6,7 @@ function App() {
 
   const [count, setCount] = useState(1);
   const [profile, setProfile] = useState({});
+  const [dark, setDark] = useState(false);
 
   useEffect(() => {
       axios.get(`https://jsonplaceholder.typicode.com/users/${count}`)
@@ -13,15 +14,39 @@ function App() {
             setProfile(response.data);
         })
         .catch(error => console.log(error))
-  }, [count])
+  }, [count]);
+
+  const toggleDark = () => {
+      setDark(!dark);
+
+      if(!dark) 
+        document.body.classList.add('bg-dark', 'text-white');
+      else 
+        document.body.classList.remove('bg-dark', 'text-white');
+      
+  };
+
+  const classBtnTheme = dark ? 'btn-light' : 'btn-dark';
+  const textBtnTheme = dark ? 'White mode' : 'Dark mode';
+
+  // Memo memorizes a value and prevents the component from re-rendering unless count state changes.
+  const memoizedValue = useMemo(() => {
+    console.log('isOverTen function');
+    return count > 10;
+  }, [count]);
+  
+  console.log(memoizedValue);
+
 
   return (
     <div className="container">
       <h1 className="text-center">useMemo()</h1>
 
+      {memoizedValue && <div className='alert alert-danger' role="alert">Limit reached</div>}
+
       <button className="btn btn-info mb-3" onClick={() => setCount(count + 1)}>Increment {count}</button>
 
-      <button className="btn btn-dark mb-3 float-end">Dark Mode</button>
+      <button className={`btn ${classBtnTheme} mb-3 float-end`} onClick={toggleDark}>{textBtnTheme}</button>
 
       <Profile count={count} profile={profile} />
     </div>
--- more-hooks/src/components/Profile.jsx
+++ more-hooks/src/components/Profile.jsx
@@ -1,14 +1,18 @@
+import { memo } from 'react';
+
 function Profile({profile, count}) {
+    console.log('Profile Component')
+
     return (
         <div>
             <ul className="list-group">
-                <li className="list-group-item"><p className="h2">User ID: {count}</p> </li>
-                <li className="list-group-item">Name: {profile.name}</li>
-                <li className="list-group-item">Username: {profile.username}</li>
-                <li className="list-group-item">Email: {profile.email}</li>
+                <li className="list-group-item"><p className="h2">User ID: #{count}</p> </li>
+                <li className="list-group-item"><strong>Name:</strong> {profile.name}</li>
+                <li className="list-group-item"><strong>Username:</strong> {profile.username}</li>
+                <li className="list-group-item"><strong>Email:</strong>  {profile.email}</li>
             </ul>
         </div>
     )
 }
 
-export default Profile;
+export default memo(Profile);
