f77e6204bdd9417255d0564375ea0690c38fbc1a
Wallmonger <nicolas.berrier360@gmail.com>
2023-10-11 18:34:29 +0200

    useImperativeHandle

--- more-hooks/src/App.js
+++ more-hooks/src/App.js
@@ -4,16 +4,21 @@ import './App.css';
 
 const App = () => {
 
+  const date = new Date();
+
   const newInputRef = useRef();
 
-  const focusInput = () => {
-    newInputRef.current.focus();
+  const focusInput = (val) => {
+    newInputRef.current.focus(val);
+    newInputRef.current.countMax();
+
+    console.log(newInputRef.current);
   }
 
   return (
     <div className='App'>
-      <NewInput name="newer" ref={newInputRef} />
-      <button onClick={focusInput}>Focus</button>
+      <NewInput ref={newInputRef} />
+      <button onClick={() => focusInput(date.getDay())}>Focus</button>
     </div>
     
   );
--- more-hooks/src/components/NewInput.jsx
+++ more-hooks/src/components/NewInput.jsx
@@ -1,14 +1,36 @@
-import { useState, forwardRef } from 'react';
+import { useState, forwardRef, useImperativeHandle, useRef } from 'react';
 
 const NewInput = forwardRef((props, ref) => {
+
+    const inputRef = useRef();
+
     const [count, setCount] = useState(0);
 
-    console.log(props.name, ref);
+    useImperativeHandle(ref, () => (
+        {
+            focus: (val) => {
+                // If prevCount == 4
+                if (count === 4) {
+                    const colors = ['red', 'blue', 'orange', 'green', 'pink','purple', 'yellow'];
+
+                    const style = `4px solid ${colors[val]}`;
+
+                    inputRef.current.style.border = style;
+                }
+            },
+
+            countMax: () => {
+                if(count < 5)
+                    setCount(count + 1);
+            }
+        }
+    ));
+
 
     return (
         <div>
             <h1>{count}</h1>
-            <input type="text" ref={ref}/>
+            <input type="text" ref={inputRef}/>
         </div>
     )
 });
