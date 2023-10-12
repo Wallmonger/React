8d23b84db18b2e8103035addf34cdb269d2270f7
Wallmonger <nicolas.berrier360@gmail.com>
2023-10-12 15:59:37 +0200

    performance.now()

--- more-hooks/src/App.js
+++ more-hooks/src/App.js
@@ -1,11 +1,11 @@
-import Box from './components/Box';
+import Box from './components/hooks/useLayoutEffect/Box';
 import './App.css';
 
 const App = () => {
   return (
     <div className='App'>
       <h1>useLayoutEffect</h1>
-      
+
       <Box />
     </div>
   )
