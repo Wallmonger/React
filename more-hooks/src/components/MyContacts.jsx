537f818e5b301dff5606757d0ad4bbcd6a67e30e
Wallmonger <nicolas.berrier360@gmail.com>
2023-10-17 17:38:42 +0200

    placeholder

--- more-hooks/src/components/MyContacts.jsx
+++ more-hooks/src/components/MyContacts.jsx
@@ -1,23 +1,53 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import useUpdateDocTitle from './hooks/useUpdateDocTitle';
 
 import Search from "./Search";
 
 const MyContacts = () => {
 
+    
+    const [users, setUsers] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
     const [search, setSearch] = useState('');
+    
+    // CustomHook
     useUpdateDocTitle(search);
+    
+    console.log(users);
 
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/users`)
+            .then(response => response.json())
+            .then(json => {
+                setUsers(json);
+                setIsLoading(false);
+            })
+            .catch(err => console.log(err.message));
+    }, []);
+    
     const handleChange = e => {
         setSearch(e.target.value);
     }
 
+    const msgDisplay = (msg, color) => {
+        return (
+            <p style={{textAlign: 'center', color: color}}>{msg}</p>
+        )
+    }
+    
 
     return (
-        <Search
-            searchStr={search} 
-            searchHandler={handleChange}
-        />
+        <>
+            {
+                isLoading ? msgDisplay('Please wait ...', 'red') : 
+                <Search
+                    searchStr={search} 
+                    searchHandler={handleChange}
+                /> 
+            }
+           
+        
+        </>
     )
 }
 
